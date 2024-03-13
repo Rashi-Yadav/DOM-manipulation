61bd2f6eceac4f277fa4cbb6abf5766e30a6c81f
Rashi-Yadav <rashi04yadav@gmail.com>
2024-03-13 20:33:28 +0530

    getElementById

---
+++ index.js
@@ -0,0 +1,14 @@
+//getElementById method 
+const heading = document.getElementById('main-heading');
+heading.textContent = 'Fruit World';
+heading.style.color = 'orange';
+
+const header = document.getElementById('header');
+header.style.backgroundColor = 'green';
+header.style.borderBottom = '3px solid orange';
+
+const h2 = document.getElementById('basket-heading');
+h2.style.color = 'green';
+
+var thanksElement = document.getElementById('thanks');
+thanksElement.innerHTML = '<p>Please visit us again</p>';
