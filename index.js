3294f546127e7ac6175fa2c2fe577edd5d8773da
Rashi-Yadav <rashi04yadav@gmail.com>
2024-03-14 20:46:25 +0530

    getElementsByClassName

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 //getElementById method 
+function elementById(){
 const heading = document.getElementById('main-heading');
 heading.textContent = 'Fruit World';
 heading.style.color = 'orange';
@@ -12,3 +13,13 @@ h2.style.color = 'green';
 
 var thanksElement = document.getElementById('thanks');
 thanksElement.innerHTML = '<p>Please visit us again</p>';
+}
+
+//getElementsByClassName method
+function elementsByClassName(){
+const fruit = document.getElementsByClassName('fruit');
+fruit[2].style.backgroundColor = 'yellow'
+for(let i = 0;i<fruit.length;i++){
+  fruit[i].style.fontWeight = 'bold'
+}
+}
