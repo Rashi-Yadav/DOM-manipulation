e1008221096a7574595ea89d35467c90afc4acfa
Rashi-Yadav <rashi04yadav@gmail.com>
2024-03-14 21:19:40 +0530

    getElementsByTagName

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const fruit = document.getElementsByClassName('fruit');
 fruit[2].style.backgroundColor = 'yellow'
 for(let i = 0;i<fruit.length;i++){
   fruit[i].style.fontWeight = 'bold'
-}
+ }
 }
 
 // getElementsByTagName method
